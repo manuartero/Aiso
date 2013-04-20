786510a035b637587ed2e9fab41a36c3b9e1334d
manutero <manutero@gmail.com>
2013-04-20 19:26:49 +0200

    trying to remove .ko.cmd files

--- p1/.p1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/manutero/Aiso/p1/p1.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-2.6.32-21-generic/scripts/module-common.lds --build-id -o /home/manutero/Aiso/p1/p1.ko /home/manutero/Aiso/p1/p1.o /home/manutero/Aiso/p1/p1.mod.o
--- p1/.p1.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/manutero/Aiso/p1/p1.o := ld -m elf_i386   -r -o /home/manutero/Aiso/p1/p1.o /home/manutero/Aiso/p1/clipboard.o /home/manutero/Aiso/p1/main.o 
