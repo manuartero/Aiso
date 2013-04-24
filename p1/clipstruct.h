c1932bd89a9596b44faab750fe1267c912b469b3
antonioIrizar <antonioirizar@gmail.com>
2013-04-24 02:38:10 +0200

    arreglado mi_atoi para que de error cuando metes una letra y no un numero

--- p1/clipstruct.h
+++ p1/clipstruct.h
@@ -5,7 +5,7 @@
 
 struct clipstruct {
     unsigned int id;
-    unsigned int num_elem;
+    unsigned int num_elem; // numero de elementos que tiene el buffer en ese momento
     char* buffer;
     struct list_head lista;
 };
--- p1/utiles.c
+++ p1/utiles.c
@@ -7,24 +7,25 @@ int mi_atoi(const char* p)
     int numero = 0;
     
     while (1) {
-      numero_leido = p[i];
-      
-      // FIXME : no funciona la comprobacion
-      /*
-      if ( (numero_leido < 48) || (numero_leido > 57) ) {
-        return -1;
-      }
-      */
+      numero_leido = (int)p[i];
         
       if (numero_leido == 10) {
         return numero;
       } 
+      
+      if ( (numero_leido< 48) || (numero_leido > 57) ) {
+        printk(KERN_INFO "No introduciste un numero\n",p);
+        return -1;
+      }
+      
       numero_leido = numero_leido - 48;
+      
       if (i == 0) {
         numero = numero_leido;
       } else {
         numero = numero * 10 + numero_leido;
       }
+      
       i++;
     }
 }
